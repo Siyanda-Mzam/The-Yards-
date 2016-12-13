b25cbc900db0c3a8883ead4d7662021a4a078c75
Siyanda-Mzam <dot.netntoz@google.com>
2016-12-13 11:26:18 +0200

    Form set up with logic for interactivity.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -30,8 +32,11 @@ app.use(express.static(path.join(__dirname, '/public/')));
 
 app.use('/', index);
 app.use('/home', index);
+app.use('/staff', index);
 app.use('/logout', index);
+app.use('/tenants', index);
 app.use('/applications', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/form-content.js
@@ -0,0 +1,86 @@
+'use strict'
+
+var i = 1;
+var cache;
+$("#residence_details").addClass("fade");
+$("#education_details").addClass("fade");
+$("#contact_details").addClass("fade");
+$("#residence_type_and_choice").addClass("fade");
+$("#payment_options").addClass("fade");
+$("#go_back").addClass("fade");
+$("#nsfas_form").addClass("fade");
+$("#cash_form").addClass("fade");
+$("#done").addClass("fade");
+$('#continue').click(function (e) {
+    e.preventDefault();
+    if (i == 1) {
+        $("#go_back").removeClass("fade");
+        $("#personal_details").addClass("fade");
+        $("#education_details").removeClass("fade");
+    }
+    else if (i == 2) {
+        $("#education_details").addClass("fade");
+        $("#contact_details").removeClass("fade");
+    }
+    else if (i == 3) {
+        $("#contact_details").addClass("fade");
+        $("#residence_type_and_choice").removeClass("fade");
+    }
+    else if (i == 4) {
+        $("#residence_type_and_choice").addClass("fade");
+        $("#payment_options").removeClass("fade");
+        if (cache) {
+            var str = "#" + cache + "_form";
+            $(str).removeClass("fade");
+        }
+        $("#continue").addClass("fade");
+        $("#done").removeClass("fade");
+    }
+    if (i <= 4)
+        i++;
+});
+$('#go_back').click(function (e) {
+    e.preventDefault();
+    $("#continue").removeClass("fade");
+    $("#done").addClass("fade");
+    $("#nsfas_form").addClass("fade");
+    $("#cash_form").addClass("fade");
+    if (i == 2) {
+        $("#personal_details").removeClass("fade");
+        $("#education_details").addClass("fade");
+    }
+    else if (i == 3) {
+        $("#education_details").removeClass("fade");
+        $("#contact_details").addClass("fade");
+    }
+    else if (i == 4) {
+        $("#contact_details").removeClass("fade");
+        $("#residence_type_and_choice").addClass("fade");
+    }
+    else if (i == 5) {
+        $("#residence_type_and_choice").removeClass("fade");
+        $("#payment_options").addClass("fade");
+    }
+    i--;
+    if (i == 1) {
+        $("#go_back").addClass("fade");
+    }
+});
+
+function poption(val) {
+    console.log("This is the value for val: %s", val);
+    if (val.value === "nsfas") {
+        $('#cash_form').addClass("fade");
+        $("#nsfas_form").removeClass("fade");
+    }
+    else if (val.value === "other") {
+        // Display a generic form that can capture information we care about.
+        // This is going to be data generated from a database schema or something of that nature. 
+        // I am not sure about the information that will be put in the other form.
+    }
+    else if (val.value === "cash") {
+        $("#nsfas_form").addClass("fade");
+        $("#cash_form").removeClass("fade");
+    }
+    cache = val.value;
+};
